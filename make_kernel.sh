0428af240db5d2fdfe28dcae5cacaa0b99a70dd6
Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>
2025-09-16 16:05:32 +0100

    Fix prepare and version length

--- make_kernel.sh
+++ make_kernel.sh
@@ -4,7 +4,6 @@
 BASENAME=$(basename "${0}")
 BASEPATH=$(dirname "${0}")
 CURPATH=$(pwd)
-TMPNAME=".tmp"
 
 function fatal() {
 	echo "FATAL ERROR: $@"
@@ -17,6 +16,7 @@ GIT=$(which git 2>/dev/null) || fatal "I need git!"
 MAKE=$(which make 2>/dev/null) || fatal "I need make!"
 
 # Process args
+MK_PREPARE=0
 MK_IMAGE=0
 MK_MODULES=0
 MK_INSTALL=0
@@ -26,6 +26,10 @@ while (( "$#" )); do
 	ARG1=$(echo "${1}" | awk '{$1=$1;print}')
 	ARG2=$(echo "${2}" | awk '{$1=$1;print}')
 	case "${ARG1}" in
+                --prepare)
+                        MK_PREPARE=1
+                        shift ;;
+
 		--image)
                         MK_IMAGE=1
                         shift ;;
@@ -39,6 +43,7 @@ while (( "$#" )); do
                         shift ;;
 
                 --full)
+                        MK_PREPARE=1
                         MK_IMAGE=1
                         MK_MODULES=1
                         MK_INSTALL=1
@@ -59,8 +64,6 @@ while (( "$#" )); do
 done
 
 function cleanup() {
-        echo "Cleanup..."
-        rm -rf ${TMPNAME}.*
         popd || exit 1
 }
 
@@ -74,9 +77,7 @@ pushd "${MK_PATH}" || fatal "pushd(${MK_PATH}) failed"
 [ -f ".config" ] || fatal "No .config in pwd"
 
 # Defaults
-MK_FILE_SRC="Makefile"
-MK_FILE_TMP=$(mktemp "${TMPNAME}.XXXXXX")
-MK_ARCH=$(uname -m)
+MK_FILE="Makefile"
 MK_NPROCS=$(${NPROC} --all)
 
 # Get some version information
@@ -90,30 +91,32 @@ else
 fi
 
 # Generate new version information
-EXTRA_VERSION=$(grep -E "^EXTRAVERSION" "${MK_FILE_SRC}" | sed -e 's/^EXTRAVERSION = //g')
-BUILD_VERSION="${EXTRA_VERSION}.${GIT_HASH}(${GIT_BRANCH}).${MK_ARCH}"
-
-# Write the version info
-cp -f "${MK_FILE_SRC}" "${MK_FILE_TMP}" || fatal "Copy Makefile failed"
-sed -i "s@^EXTRAVERSION.*@EXTRAVERSION = ${BUILD_VERSION}@" "${MK_FILE_TMP}"
+EXTRA_VERSION=$(grep -E "^EXTRAVERSION" "${MK_FILE}" | sed -e 's/^EXTRAVERSION = //g')
+EXTRA_VERSION="${EXTRA_VERSION}.${GIT_HASH}.${GIT_BRANCH}"
 
 # Run a build
 NUM_PROCS=$(( "${MK_NPROCS}" + 0 ))
-echo "TMP_MAKEFILE = ${MK_FILE_TMP}"
-echo "EXTRAVERSION = ${BUILD_VERSION}"
+MK_COMMAND="${MAKE} EXTRAVERSION=${EXTRA_VERSION} -j${NUM_PROCS}"
+MK_VERSION=$(${MK_COMMAND} -s kernelrelease)
+
+echo "MK_VERSION=${MK_VERSION}"
 echo "MK_IMAGE=${MK_IMAGE} MK_MODULES=${MK_MODULES} MK_INSTALL=${MK_INSTALL} MK_CLEAN=${MK_CLEAN} NUM_PROCS=${NUM_PROCS}"
 sleep 1
 
+if [ "${MK_PREPARE}" -gt 0 ]; then
+        ${MK_COMMAND} oldconfig || fatal "make oldconfig failed"
+        ${MK_COMMAND} prepare || fatal "make prepare failed"
+fi
 if [ "${MK_IMAGE}" -gt 0 ]; then
-        ${MAKE} -f "${MK_FILE_TMP}" -j${NUM_PROCS} || fatal "make image failed"
+        ${MK_COMMAND} || fatal "make image failed"
 fi
 if [ "${MK_MODULES}" -gt 0 ]; then
-        ${MAKE} -f "${MK_FILE_TMP}" -j${NUM_PROCS} modules || fatal "make modules failed"
+        ${MK_COMMAND} modules || fatal "make modules failed"
 fi
 if [ "${MK_INSTALL}" -gt 0 ]; then
-        ${MAKE} -f "${MK_FILE_TMP}" -j${NUM_PROCS} modules_install || fatal "make modules_install failed"
-        ${MAKE} -f "${MK_FILE_TMP}" -j${NUM_PROCS} install || fatal "make install failed"
+        ${MK_COMMAND} modules_install || fatal "make modules_install failed"
+        ${MK_COMMAND} install || fatal "make install failed"
 fi
 if [ "${MK_CLEAN}" -gt 0 ]; then
-        ${MAKE} -f "${MK_FILE_TMP}" -j${NUM_PROCS} clean || fatal "make clean failed"
+        ${MK_COMMAND} clean || fatal "make clean failed"
 fi
